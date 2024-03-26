94f672d1234565ecd9f0c9905e826108ed2f8a41
covvboyy <jamison.bassett@gmail.com>
2024-03-26 12:29:31 +0100

    healthbar shows numerical values

--- godot4version/Entities/UI/HealthBar.gd
+++ godot4version/Entities/UI/HealthBar.gd
@@ -1,5 +1,7 @@
 extends TextureProgressBar
 
+@onready
+var label = $Amount
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	PartyManager.hp_change.connect(update_hp)
@@ -12,3 +14,4 @@ func set_hp():
 func update_hp(new_hp:int):
 	var tween = create_tween()
 	tween.tween_property(self, "value", new_hp, .4).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	label.text = str(new_hp, "/", PartyManager.party_hp)
